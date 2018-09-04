7bc425800070e809b664b541c0978ef3a8a59047
Marija Kovacevic <Marija.Kovacevic@endava.com>
2018-09-04 13:42:27 +0200

    add question service logic for question form

--- src/app/questions/question.service.ts
+++ src/app/questions/question.service.ts
@@ -1,12 +1,29 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 
+import { Question } from '../shared/question.model';
+
 @Injectable()
 export class QuestionService {
 	// questionTypes: string[] = ['Single Choice', 'Multiple Choice', 'Text', 'Connecting', 'Order'];
-	newQuestion: {}
-	
+	newQuestion: Question = {
+		id: null,
+		question: '',
+		category: '',
+		difficulty: '',
+		type: '',
+		answers: []
+	};
+
 	constructor(private router: Router) { }
 
+	createQuestion(id: number, question: string, category: string, difficulty: string, type: string, answers: object[]) {
+		this.newQuestion.id = id;
+		this.newQuestion.question = question;
+		this.newQuestion.category = category;
+		this.newQuestion.difficulty = difficulty;
+		this.newQuestion.type = type;
+		this.newQuestion.answers = answers;
+	}
 
 }
--- src/app/questions/question/question.component.ts
+++ src/app/questions/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { QuestionService } from '../question.service';
 
 
 @Component({
@@ -13,20 +14,29 @@ export class QuestionComponent implements OnInit {
 	categories: string[] = ['JavaScript', 'Java', 'PHP'];
 	questionTypes: string[] = ['Single Choice', 'Multiple Choice', 'Text', 'Connecting', 'Order'];
 
-	constructor() { }
+	constructor(private questionService: QuestionService) { }
 
 	ngOnInit() {
 		this.questionForm = new FormGroup({
 			'question': new FormControl(null),
-			'category': new FormControl(),
-			'difficulty': new FormControl(),
-			'type': new FormControl()
+			'category': new FormControl(null),
+			'difficulty': new FormControl(null),
+			'type': new FormControl(null)
 		})
 	}
 
 	onSubmitQuestion() {
 		// console.log(this.questionForm.controls)
-		console.log(this.questionForm.controls.question.value)
+		const id = Math.floor(Math.random() * 100000) + 1;
+		const question = this.questionForm.controls.question.value;
+		const category = this.questionForm.controls.category.value;
+		const difficulty = this.questionForm.controls.difficulty.value;
+		const type = this.questionForm.controls.type.value;
+		const answers = null;
+
+		this.questionService.createQuestion(id, question, category, difficulty, type, answers)
+		// console.log(this.questionForm.controls.question.value)
+		console.log(this.questionService.newQuestion)
 	}
 
 }
