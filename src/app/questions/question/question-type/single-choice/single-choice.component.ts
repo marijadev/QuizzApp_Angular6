8d0df16e5dcd87fb701632a62d61c377499a0dc7
Marija Kovacevic <Marija.Kovacevic@endava.com>
2018-09-05 16:09:36 +0200

    add single-choice dynamically added input

--- src/app/questions/question/question-type/single-choice/single-choice.component.ts
+++ src/app/questions/question/question-type/single-choice/single-choice.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
-  selector: 'app-single-choice',
-  templateUrl: './single-choice.component.html',
-  styleUrls: ['./single-choice.component.scss']
+	selector: 'app-single-choice',
+	templateUrl: './single-choice.component.html',
+	styleUrls: ['./single-choice.component.scss']
 })
 export class SingleChoiceComponent implements OnInit {
+	answerForm: FormGroup;
+	constructor(private fb: FormBuilder) { }
 
-  constructor() { }
+	ngOnInit() {
+		this.answerForm = this.fb.group({
+			title: [],
+			answers: this.fb.array([this.fb.group({ point: '' })])
+		})
+	}
 
-  ngOnInit() {
-  }
+	get answers() {
+		return this.answerForm.get('answers') as FormArray;
+	}
+
+	addAnswer() {
+		this.answers.push(this.fb.group({ point: '' }));
+	}
+
+	deleteAnswer(index) {
+		if (index !== 0) {
+			this.answers.removeAt(index);
+		}
+	}
 
 }
