71c67ab8d340047100cac6d4efea0034e6e6ee72
Marija Kovacevic <Marija.Kovacevic@endava.com>
2018-09-27 13:50:21 +0200

    add user test animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRouting } from './app-routing.module';
@@ -41,7 +42,8 @@ import { TestComponent } from './tests/test/test.component';
 		AuthModule,
 		CoreModule,
 		AppRouting,
-		HttpClientModule
+		HttpClientModule,
+		BrowserAnimationsModule
 	],
 	providers: [
 		{
--- src/app/tests/test-type/test-type.component.ts
+++ src/app/tests/test-type/test-type.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { SlideInOutAnimation } from '../../shared/animations';
 
 @Component({
-  selector: 'app-test-type',
-  templateUrl: './test-type.component.html',
-  styleUrls: ['./test-type.component.scss']
+	selector: 'app-test-type',
+	templateUrl: './test-type.component.html',
+	styleUrls: ['./test-type.component.scss'],
+	animations: [SlideInOutAnimation]
 })
 export class TestTypeComponent implements OnInit {
+	animationStateDifficulty = 'out ';
+	animationStateCategory = 'out ';
+	animationStateDifficultyCategory = 'out ';
+	constructor() { }
 
-  constructor() { }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
-
+	onTypeSelect(headline: string) {
+		if (headline === 'difficulty') {
+			this.animationStateCategory = this.animationStateCategory === 'out' && 'out';
+			this.animationStateDifficultyCategory = this.animationStateDifficultyCategory === 'out' && 'out';
+			this.animationStateDifficulty = this.animationStateDifficulty === 'out' ? 'in' : 'out';
+		} else if (headline === 'category') {
+			this.animationStateDifficulty = this.animationStateDifficulty === 'out' && 'out';
+			this.animationStateDifficultyCategory = this.animationStateDifficultyCategory === 'out' && 'out';
+			this.animationStateCategory = this.animationStateCategory === 'out' ? 'in' : 'out';
+		} else if (headline === 'difficulty-category') {
+			this.animationStateDifficulty = this.animationStateDifficulty === 'out' && 'out';
+			this.animationStateCategory = this.animationStateCategory === 'out' && 'out';
+			this.animationStateDifficultyCategory = this.animationStateDifficultyCategory === 'out' ? 'in' : 'out';
+		}
+	}
 }
